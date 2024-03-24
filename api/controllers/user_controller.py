a2ddc1a1de46154e356f4f267e090ed15dd16a8f
Rodrigocambraia14 <rodrigocambraia.jr@gmail.com>
2024-03-23 22:50:47 -0300

    implementação do swagger

--- api/controllers/user_controller.py
+++ api/controllers/user_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 
+user_controller = Blueprint('user_controller', __name__)
 
 @user_controller.route("/user/add", methods=['POST'])
 def user_add():
--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
-from api.controllers import user_controller
-
+from flask import Flask, jsonify
+from api.controllers.user_controller import user_controller
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+import json
 
 app = Flask(__name__)
 
 app.register_blueprint(user_controller)
+
+CORS(app)
+
+# Configure Swagger UI
+SWAGGER_URL = '/swagger'
+API_URL = 'http://127.0.0.1:5000/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Sample API"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route('/swagger.json')
+def swagger():
+    with open('swagger.json', 'r') as f:
+        return jsonify(json.load(f))
 
 app.run()
